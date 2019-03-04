085890cd07da8d6dd3e961345f2d8cdf2db52dbf
Jonrosario5 <Jonathan.Rosario550@gmail.com>
2019-03-04 12:02:49 -0800

    Added Axios to dependencies and corrected file route in Home.JS in lines 2-4

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Header from "./component/Header";
-import NavBar from "./component/NavBar";
-import About from "./component/About";
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import About from "./components/About";
 
 class Home extends Component {
   render() {
