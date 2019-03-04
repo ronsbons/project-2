4b42a509e678c080f968637de2ff241be9689ea2
Jonrosario5 <Jonathan.Rosario550@gmail.com>
2019-03-04 14:35:21 -0800

    Added proper indents to NavBar.Js JSX syntax.Also placed login/signupmodale in a section

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,46 +10,50 @@ class NavBar extends Component {
         return(
             <div>
                 Login/Signup
-                <div className="login">
-                    <h4>Login</h4>
-                    <form className="login" >
-                    <input
-                    type="text"
-                    placeholder="Username"
-                    />
-                    <input
-                    type="text"
-                    placeholder="Password"
-                    />
-                    <button className="loginButton"
-                    type="submit"
-                    >Login
-                    </button>
-                    </form> 
-                </div>
-                <div className="signup">
-                    <h4>Signup</h4>
-                    <form className="signup" >
-                    <input
-                    type="text"
-                    placeholder="UserName"
-                    />
-                    <input
-                    type="text"
-                    placeholder="Password" 
-                    />
-                    <input 
-                    type="text"
-                    placeholder="Confirm Password"
-                    />
-                    <button className="signupButton"
-                    type="submit"
-                    >Sign up
-                    </button>
-                    </form>
-                </div>
+                <section>
+                    <div className="login">
+                        <h4>Login</h4>
+                        <form className="login" >
+                            <input
+                                 type="text"
+                                 placeholder="Username"
+                                />
+                             <input
+                                    type="text"
+                                    placeholder="Password"
+                                    />
+                             <button className="loginButton"
+                                type="submit"
+                                >Login
+                                </button>
+                            </form> 
+                        </div>
+                        <div className="signup">
+                            <h4>Signup</h4>
+                            <form className="signup" >
+                                <input
+                                    type="text"
+                                    placeholder="UserName"
+                                 />
+                                <input
+                                    type="text"
+                                    placeholder="Password" 
+                                 />
+                                 <input 
+                                    type="text"
+                                    placeholder="Confirm Password"
+                                    />
+                                    <button className="signupButton"
+                                        type="submit"
+                                        >Sign up
+                                        </button>
+                             </form>
+                            </div>
+
+                    </section>
+                
                 Logout/ Regular NavBar
-            </div>
+                </div>
             
         )
     }
