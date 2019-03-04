8afb70fbdbd6e104558237e3a9ba722703e4d4b6
Jonrosario5 <Jonathan.Rosario550@gmail.com>
2019-03-04 11:59:09 -0800

    Added file structure for components

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import Header from "./component/Header";
+import NavBar from "./component/NavBar";
+import About from "./component/About";
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
+      <NavBar/>
+      <Header />
+      <About />
 
       </div>
     );
