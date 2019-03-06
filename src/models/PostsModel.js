46a997b869d175b2b342c398135361115c4b3be4
lvmor <lvm.lspa@gmail.com>
2019-03-06 07:33:53 -0800

    tests axios request is working

--- src/models/PostsModel.js
+++ src/models/PostsModel.js
@@ -5,6 +5,7 @@ const endPoint = `http://localhost:3001/api/posts`;
 class PostsModel {
   static allPosts() {
     let request = axios.get(endPoint);
+    console.log("axios request", request);
     return request;
   }
 }
