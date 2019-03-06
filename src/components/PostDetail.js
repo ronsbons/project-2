2003235ce3cfa9d03989608e6ae32caf20010b2d
lvmor <lvm.lspa@gmail.com>
2019-03-06 07:23:57 -0800

    creates post detail component with title details for public profile modal

---
+++ src/components/PostDetail.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class PostDetail extends Component {
+    
+    render() {
+        return (
+            <div>
+                <h1>{this.props.post.postTitle}</h1>
+                <h2>{this.props.post.user}</h2>
+                <p>{this.props.post.postContent}</p>
+            </div>
+        );
+    }
+}
+
+export default PostDetail;
