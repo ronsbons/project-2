1e8aecc5b27093e48e06c91af4c8b685b7cd2a07
e-morales <e.morales@digitalnest.org>
2019-03-06 15:04:17 -0800

    edited user related components

--- src/container/UserContainer.js
+++ src/container/UserContainer.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import UserModel from '../models/UserModel.js';
+import UserComponent from '../components/UserComponent';
+
 
 class UserContainer extends Component {
+    state = {
+        user: {
+            userPhoto: '',
+            userFullName: '',
+            userCity: '',
+            userJoinDate: ''
+        }
+
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData = () => {
+        console.log('triggered fetch')
+        UserModel.allUsers().then((res) => {
+            console.log(res)
+            this.setState({user: res.data[0]});
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }
+
     render(){
         return(
             <div>
-                This is the UserContainer
+                <h2>User Profile</h2>
+                <UserComponent user={this.state.user} />
             </div>
         )
     }
--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -4,7 +4,9 @@ const endPoint = `http://localhost:3001/api/users`;
 
 class UserModel {
   static allUsers() {
+    console.log('getting users')
     let request = axios.get(endPoint);
+    console.log(request)
     return request;
   }
 }
