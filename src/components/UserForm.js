aef5ddc91392c697dd454cf2af6c931965aa8160
lvmor <lvm.lspa@gmail.com>
2019-03-07 09:43:47 -0800

    creates form functionality

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react';
 
-class UserForm extends Component {
-    render() {
-        return (
-            <div>
-                <form>
-                    <input type="text" placeholder="Name" />
-                        
-                    
-                    <button type='submit'>Edit</button>
-                </form>
-            </div>
-        );
+class UpdateUserForm extends Component {
+  state={
+    user: {
+      userFullName: "",
+      userCity: ""
     }
+  }
+
+  componentDidMount() {
+    console.log('USER');
+    console.log(this.props.user);
+    this.setState({
+        user: this.props.user
+    });
+  }
+
+  onInputChange = (event) => {
+    let user = this.state.user;
+    user[event.target.name] = event.target.value;
+
+    this.setState({
+      user: user
+    });
+
+    console.log(this.state.user)
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault()
+    let user = this.state.user
+    this.props.updateUser(user, '5c80599dd1a625547d631928')
+    user = ""
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onFormSubmit}>
+          <input 
+          name="userFullName"
+          onChange={this.onInputChange}
+          type="text"
+          placeholder="Name"
+          value={(this.state.user.userFullName) || ""}
+          />
+          <input 
+          name="userCity"
+          onChange={this.onInputChange}
+          type="text"
+          placeholder="Current City"
+          value={(this.state.user.userCity) || ""}
+          />
+          <button type="submit">Change</button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default UserForm;
+export default UpdateUserForm;
