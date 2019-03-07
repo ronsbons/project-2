ca53608007cc19d5c7fb98a19897c276d8e408eb
Jonrosario5 <Jonathan.Rosario550@gmail.com>
2019-03-06 17:53:54 -0800

    removed react-materialized

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,6 @@ class NavBar extends Component {
         let navBarItems = [];
         if(this.props.isLoggedIn){
             navBarItems.push(
-<<<<<<< HEAD
                 <li>
                 <button
                     className="logOutBtn"
@@ -20,11 +19,6 @@ class NavBar extends Component {
                 </li>
 
             );
-=======
-            <NavItem key={1} href="/logout" onClick={this.props.handleLogout}>
-                Logout
-            </NavItem>);
->>>>>>> 03f7d2e94eb69133f9bb33fe8fc680b6bff04381
         } else{
             navBarItems.push(
                 <li>
