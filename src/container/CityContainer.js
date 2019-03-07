8824844dc57bdadebbe1a5e9531d80e02cf6a2cb
ronsbons <ronnilouie@gmail.com>
2019-03-06 22:12:10 -0800

    begins filling out CityContainer

--- src/container/CityContainer.js
+++ src/container/CityContainer.js
@@ -1,14 +1,33 @@
 import React, {Component} from 'react';
-
+import CityList from '../components/CityList.js';
+import CityPage from '../components/CityPage.js';
+import Axios from 'axios';
 
 class CityContainer extends Component {
-    render(){
-        return(
-            <div>
-                CityContainer
-            </div>
-        )
-    }
+  state = {
+    cities: []
+  };
+
+  componentDidMount() {
+    Axios.get('http://localhost:3001/api/cities').then( (response) => {
+      this.setState({
+        cities: response.data.cities,
+      });
+    }).catch( (error) => {
+      console.log('retrieiving all cities error: ', error);
+    });
+  };
+
+  render(){
+    return(
+      <div>
+        CityContainer
+        <CityList cities={this.state.cities} />
+        {/* [] MAY HAVE TO MOVE CITYPAGE COMPONENT INSIDE CITYLIST, BECAUSE A CLICK ON A CITY NAME WILL HAVE TO CHANGE THE STATE OF CITYPAGE TO SHOW THE CITY THAT WAS CLICKED ON. */}
+        <CityPage />
+      </div>
+    )
+  }
 }
 
 export default CityContainer;
