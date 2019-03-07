ed1f27e703739d855df19fdea3d3fd51d50935eb
ronsbons <ronnilouie@gmail.com>
2019-03-07 10:21:22 -0800

    begins logic to change city upon click

--- src/components/City.js
+++ src/components/City.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
 class City extends Component {
+  state = {
+    city: 'San Francisco',
+  };
+
+  changeCity = (event) => {
+    event.preventDefault();
+    this.setState({
+      // [] HOW TO GET CITYNAME FROM <A HREF>?
+      city: event.target.textContent,
+    });
+  };
+
   render() {
     return (
       <div>
         <h5>City component</h5>
-        <a href="#">{this.props.city.cityName}</a>
+        <a href="#" onClick={this.changeCity}>{this.props.city.cityName}</a>
+        <CityPage city={this.state.city} />
       </div>
     );
   }
