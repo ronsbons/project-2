d4979df3537eef12f149e83bd9bda9948c64572e
Jonrosario5 <Jonathan.Rosario550@gmail.com>
2019-03-07 16:55:57 -0800

    added modal to postdetail

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,38 +1,23 @@
 import React, { Component } from "react";
+import { Button, Header, Image, Modal } from 'semantic-ui-react';
+
  
 
 class PostDetail extends Component {
-  state= {
-    open:false
-}
-
-handleOpen = () => {
-    this.setState ({
-        open:true
-    })
-}
-
-handleClose = () => {
-    this.setState({
-        open:false
-    })
-}
 
   render() {
     return (
-      <div>
-        <button
-        onClick={this.handleOpen}
-        >
-          Edit 
-        </button>
-      
-          <h1>{this.props.post.postTitle}</h1>
-          <h2>{this.props.post.user}</h2>
-          <p>{this.props.post.postContent}</p> 
-          
-           Hi
-      </div>
+    <Modal trigger={<a>{this.props.post.postContent}</a>}>
+    <Modal.Header>City Post</Modal.Header>
+    <Modal.Content image>
+      <Image wrapped size='medium' src='https://www.aaronstoychest.co.uk/wp-content/uploads/2017/09/59754444.jpg' />
+      <Modal.Description>
+        <Header>{this.props.post.postTitle}</Header>
+        <h2>{this.props.posts.user}</h2>
+        <p>{this.props.posts.postContent}</p>
+      </Modal.Description>
+    </Modal.Content>
+  </Modal>
     );
   }
 }
