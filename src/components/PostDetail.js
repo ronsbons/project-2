915c38de969b3b034fcbcb1b4c8f8cb23eeb5532
lvmor <lvm.lspa@gmail.com>
2019-03-07 16:12:57 -0800

    removes materialize

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,40 +1,12 @@
 import React, { Component } from "react";
-import Modal from '@material-ui/core/Modal';
- 
 
 class PostDetail extends Component {
-  state= {
-    open:false
-}
-
-handleOpen = () => {
-    this.setState ({
-        open:true
-    })
-}
-
-handleClose = () => {
-    this.setState({
-        open:false
-    })
-}
-
   render() {
     return (
       <div>
-        <button
-        onClick={this.handleOpen}
-        >
-          Edit 
-        </button>
-        <Modal
-        open={this.state.open}
-        onClose={this.handleClose}
-        >
-          <h1>{this.props.post.postTitle}</h1>
-          <h2>{this.props.post.user.userEmail}</h2>
-          <p>{this.props.post.postContent}</p> */}
-        </Modal>
+        <h1>{this.props.post.postTitle}</h1>
+        <h2>{this.props.post.user.userEmail}</h2>
+        <p>{this.props.post.postContent}</p>
       </div>
     );
   }
