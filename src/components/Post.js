f187203c47131b2c483e80e11052a6a8da07489b
lvmor <lvm.lspa@gmail.com>
2019-03-07 16:11:59 -0800

    sends down post info

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
-import PostDetail from './PostDetail';
+import PostDetail from "./PostDetail";
 
 class Post extends Component {
-  showPost = (event) => {
+  showPost = event => {
     event.preventDefault();
   };
-  
+
   render() {
     return (
       <li data-posts-index={this.props.post.id}>
         <a href="#">{this.props.post.postTitle}</a>
-        <PostDetail />
+        <PostDetail post={this.props.post} />
       </li>
     );
   }
