9696808809a6b49b212cb9b43225edf1751e4dea
Jonrosario5 <Jonathan.Rosario550@gmail.com>
2019-03-09 13:20:12 -0800

    Adding Styling

--- src/models/PostsModel.js
+++ src/models/PostsModel.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const endPoint = `http://arcane-citadel-72655.herokuapp.com/api/userposts`;
+const endPoint = `http://localhost:3001/api/userposts`;
 
 class PostsModel {
   static getUserPosts(userId) {
