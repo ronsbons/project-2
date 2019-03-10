ede6557958fc46ff36da325a1ae7dbaf7e653a50
lvmor <lvm.lspa@gmail.com>
2019-03-09 19:10:49 -0800

    adds load city page functionality and adds 2 columns to page layout

--- src/container/CityContainer.js
+++ src/container/CityContainer.js
@@ -1,33 +1,58 @@
-import React, {Component} from 'react';
-import CityModel from '../models/CityModel.js';
-import CityList from '../components/CityList.js';
-import CityPage from '../components/CityPage.js';
+import React, { Component } from "react";
+import CityModel from "../models/CityModel.js";
+import CityList from "../components/CityList.js";
+import CityPage from "../components/CityPage.js";
 
 class CityContainer extends Component {
+  constructor() {
+    super();
+    this.loadCity = this.loadCity.bind(this);
+  }
   state = {
     cities: [],
-    currentCityId: sessionStorage.getItem('currentCity'),
+    currentCityId: sessionStorage.getItem("currentCity")
   };
 
   componentDidMount() {
-    CityModel.allCities().then( (response) => {
-      this.setState({
-        cities: response.data,
+    CityModel.allCities()
+      .then(response => {
+        this.setState({
+          cities: response.data
+        });
+        console.log(this.state.cities);
+      })
+      .catch(error => {
+        console.log("retrieiving all cities error: ", error);
       });
-      console.log(this.state.cities);
-    }).catch( (error) => {
-      console.log('retrieiving all cities error: ', error);
-    });
-  };
+  }
 
-  render(){
-    return(
+  loadCity() {
+    this.setState({
+      currentCityId: sessionStorage.getItem("currentCity")
+    });
+  }
+
+  render() {
+    return (
       <div className="city-container">
         CityContainer
-        <CityList cities={this.state.cities} user={this.props.user} />
-        {/* <CityPage currentCityId={this.state.currentCityId} /> */}
+        <div className="row">
+          <div className="col s4">
+            <CityList
+              cities={this.state.cities}
+              user={this.props.user}
+              loadCity={this.loadCity}
+            />
+          </div>
+          <div className="col s8">
+            <CityPage
+              currentCityId={this.state.currentCityId}
+              user={this.props.user}
+            />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
