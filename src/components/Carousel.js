913d90556e7bc182b038654d61900df5e821601c
Jonrosario5 <Jonathan.Rosario550@gmail.com>
2019-03-10 22:10:45 -0700

    added the Carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import Slider from 'react-slick';
 
 
 const Carousel = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 10000,
+        autoplaySpeed: 5000,
+        cssEase: "linear"
+   
+      };
     return (
-        <div className='frisco'>
-        </div>
-    )
+        <div>
+        <h2> Single Item</h2>
+        <Slider {...settings}>
+          <div className="frisco">
+          </div>
+          <div className="plane">
+          </div>
+          <div className="relaxing">
+        
+          </div>
+          <div className="paris">
+          </div>
+        
+        </Slider>
+      </div>
+    );
 }
 
 export default Carousel;
